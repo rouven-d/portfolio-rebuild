2b88356cf9284d1c55e84bd060abdca99e4e285d
Rouven Daunke <rouven.daunke@googlemail.com>
2021-08-15 17:48:38 +0200

    vanta background library setup, Intro section coded

--- pages/_app.js
+++ pages/_app.js
@@ -9,9 +9,28 @@ import "@fortawesome/fontawesome-free/js/brands";
 import Head from "next/head";
 import Layout from "../components/layout";
 
+// Global Style Sheet Import
 import "../styles/styles.css";
 
+// Vanta 3D Background Setup
+import { useEffect } from "react";
+
 function MyApp({ Component, pageProps }) {
+  //Setting up threeScript as a precursor to the Vanta 3D Background Library
+  useEffect(() => {
+    const threeScript = document.createElement("script");
+    threeScript.setAttribute("id", "threeScript");
+    threeScript.setAttribute(
+      "src",
+      "https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js"
+    );
+    document.getElementsByTagName("head")[0].appendChild(threeScript);
+    return () => {
+      if (threeScript) {
+        threeScript.remove();
+      }
+    };
+  }, []);
   return (
     <>
       <Head>
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,40 @@
 import Head from "next/head";
-import Nav from "../components/nav";
+import Image from "next/image";
+import logo from "../public/RDLogo.png";
+
+//Setup for Vanta background
+import { useEffect, useRef, useState } from "react";
+import NET from "vanta/dist/vanta.net.min.js";
+import * as THREE from "three";
 
 export default function Home() {
+  const [vantaEffect, setVantaEffect] = useState(0);
+  const myRef = useRef(null);
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(
+        NET({
+          el: myRef.current,
+          THREE,
+          mouseControls: true,
+          touchControls: true,
+          gyroControls: false,
+          minHeight: 200.0,
+          minWidth: 200.0,
+          scale: 1.0,
+          scaleMobile: 1.0,
+          points: 16.0,
+          maxDistance: 25.0,
+          spacing: 17.0,
+          backgroundColor: 0x0e0240,
+          color: 0xbd0fe1,
+        })
+      );
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy();
+    };
+  }, [vantaEffect]);
   return (
     <>
       <Head>
@@ -10,7 +43,23 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1>Hello Web 3.0</h1>
+        <section id="intro">
+          <div className="intro-bg" ref={myRef}>
+            <h1 className="intro-title">Rouven Daunke</h1>
+            <p className="intro-subheading">
+              Web Development, Web Design, Web 3.0
+            </p>
+            <div className="intro-logo">
+              <Image
+                src={logo}
+                alt="Rouven Daunke's Logo"
+                width={400}
+                height={400}
+              ></Image>
+            </div>
+          </div>
+          ;
+        </section>
       </main>
     </>
   );
